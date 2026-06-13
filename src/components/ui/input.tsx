3c43068296adebfeb7c36df95d7c9e7ab273cf67
anmar727 <anmar727@gmail.com>
2026-06-13 14:59:03 +0400

    Add input component

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export function Input({ className, ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
+  return <input className={cn("flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:opacity-50", className)} {...props} />;
+}
